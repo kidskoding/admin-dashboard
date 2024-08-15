a7e1690094cc712b10f204592b575d33c7789dd1
Anirudh Konidala <68204671+kidskoding@users.noreply.github.com>
2024-08-15 03:24:41 -0500

    readded postgresql db

---
+++ server/postgresdb.js
@@ -0,0 +1,19 @@
+const { Client } = require('pg');
+require('dotenv').config();
+
+const client = new Client({
+    host: 'localhost',
+    user: 'postgres',
+    password: process.env.PG_DB_PASSWORD,
+    database: 'login-system'
+});
+
+client.connect(err => {
+    if (err) {
+        console.error('PostgreSQL connection error:', err.stack);
+    } else {
+        console.log('PostgreSQL connected...');
+    }
+});
+
+module.exports = client;
